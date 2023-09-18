93af34c1f7ba2318773248778d9b2bbc5142ef08
김태호 <khtiq200@hubilon.com>
2023-09-18 14:05:24 +0900

    test 1 2 add

---
+++ app/src/main/java/com/hotta/hoho/msg/test.java
@@ -0,0 +1,7 @@
+package com.hotta.hoho.msg;
+
+public class test {
+    public test() {
+        System.out.println("ㅋㅋ");
+    }
+}
---
+++ app/src/main/java/com/hotta/hoho/msg/test2.java
@@ -0,0 +1,7 @@
+package com.hotta.hoho.msg;
+
+public class test2 {
+    public test2() {
+        System.out.println("ㅋㅋ");
+    }
+}
